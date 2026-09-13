272477dd7ea9188d5bacfec19bf58b517474a50f
FundaAgent <323139829+FundaAgent@users.noreply.github.com>
2026-09-13 10:18:56 +0000

    Remove orphaned api dead code after caelum-server removal
    WebResp and the misspelled CaulumApiClientException were only used by the
    CaelumApiClient and WeatherModule that the caelum-server removal deleted;
    nothing references them any more. Drop them and the now-empty api package.
    
    Test: ./gradlew assembleDebug compileDebugUnitTestKotlin
    Change-Id: Ia8a79a2ff53d2b5c9c8b5538553c21b785e943c6

--- app/src/main/java/org/fundamentalos/weather/api/exception/CaulumApiClientException.kt
+++
@@ -1,7 +0,0 @@
-package org.fundamentalos.weather.api.exception
-
-class CaulumApiClientException: Exception {
-    constructor(message: String): super(message)
-    constructor(cause: Throwable): super(cause)
-    constructor(message: String, cause: Throwable): super(message, cause)
-}
--- app/src/main/java/org/fundamentalos/weather/api/model/WebResp.kt
+++
@@ -1,9 +0,0 @@
-package org.fundamentalos.weather.api.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class WebResp<T>(
-    val ok: Boolean,
-    val data: T
-)
